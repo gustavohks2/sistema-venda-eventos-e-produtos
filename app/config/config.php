c597b583d3011c1a78e0f846051e0cd60441c97e
Gustavo <gustavohks2@gmail.com>
2018-05-16 22:51:37 -0300

    Adiciona arquivo de configuração :wrench:

---
+++ app/config/config.php
@@ -0,0 +1,11 @@
+<?php
+   $config = [
+      "database" => [
+         "name" => "comercio-eventos-produtos",
+         "host" => "127.0.0.1:3306",
+         "username" => "root",
+         "password" => ""
+      ],
+
+      "uploadFolder" => "/mudar/mudar"
+   ];
