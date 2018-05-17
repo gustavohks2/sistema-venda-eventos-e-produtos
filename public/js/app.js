abda8a6f3f6a0d0991b27f4c91d913c2ba3ad6ab
Gustavo <gustavohks2@gmail.com>
2018-05-16 22:19:12 -0300

    Estrutura básica de pastas e arquivos :tada:

---
+++ public/js/app.js
@@ -0,0 +1,16 @@
+const module = (function() {
+
+   DOMElements = {
+      // Elementos do modelo de objetos do documento - DOM
+   }
+
+   const init = function() {
+      console.log("Aplicação iniciada!");
+   }
+
+   return {
+      init
+   }
+})();
+
+module.init();
