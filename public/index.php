7219ffc12a1baa9d3ad38b6abde9907f05cf5ac4
Gustavo <gustavohks2@gmail.com>
2018-05-20 18:03:33 -0300

    Define a raíz do documento para a pasta public com arquivo index.php :truck:

--- public/index.php
+++ public/index.php
@@ -1,6 +1,8 @@
 <?php
    ini_set("display_errors", "On");
 
+   require_once __DIR__ . DIRECTORY_SEPARATOR . '../vendor/autoload.php';
+   require_once __DIR__ . DIRECTORY_SEPARATOR .'../core/bootstrap.php';
    
    
    
