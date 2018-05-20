db53d0b3a4291b796be63cfae06cd553b01701b9
Gustavo <gustavohks2@gmail.com>
2018-05-20 20:48:15 -0300

    Faz alterações genéricas em alguns arquivos :beers:

--- core/Route.php
+++ core/Route.php
@@ -1,10 +1,5 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 namespace Core;
 
 class Route{
@@ -42,6 +37,7 @@ class Route{
     }
     
     private function run(){
+        $found = false;
         $url = $this->getUrl();
         $urlArray = explode('/', $url);
         
