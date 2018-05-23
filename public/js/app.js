f8a3360446d7dbce2ce7442e95411c4d6eb516a9
Gustavo <gustavohks2@gmail.com>
2018-05-22 23:50:39 -0300

    Implementa fechamento de popup com a tecla ESC :sparkles:

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
    // Elementos do modelo de objetos do documento - DOM
+   popupOpen = false;
+
    DOM = {
       popup: $(".popup"),
       loginOpen: $(".js-login-open"),
@@ -9,14 +11,26 @@ $(document).ready(function() {
    const bindEvents = function() {
       DOM.loginOpen.click(openLoginPopup);
       DOM.loginClose.click(closeLoginPopup);
+      $(document).on("keyup", closeLoginPopupKeyboard);
    }
 
    const openLoginPopup = function() {
+      popupOpen = true;
       DOM.popup.addClass("active");
    }
 
    const closeLoginPopup = function() {
+      popupOpen = false;
       DOM.popup.removeClass("active");
+   }
+
+   const closeLoginPopupKeyboard = function(e) {
+      if (popupOpen) {
+         if ((e.which || e.keyCode) == 27) {
+            popupOpen = false;
+            DOM.popup.removeClass("active");
+         }
+      }
    }
 
    const init = function() {
