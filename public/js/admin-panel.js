fdb5d5170fd6650a1dd5df7dae6eb7b7949f10da
Gustavo <gustavohks2@gmail.com>
2018-06-03 22:53:55 -0300

    Implementa estilo e toggle do menu de opções :sparkles: :lipstick:

---
+++ public/js/admin-panel.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+   $(document).ready(function() {
+      // Elementos do modelo de objetos do documento - DOM
+      DOM = {
+         navMenu: $(".js-nav-menu"),
+         alertBox: $(".js-alert-box"),
+         arrowDownButton: $(".js-arrow-down-button"),
+         alertCloseButton: $(".js-alert-close-button"),
+         inputs: {
+            CEP: $("[data-input='CEP']"),
+            CPF: $("[data-input='CPF']"),
+            telefone: $("[data-input='telefone']")
+         }
+      }
+   
+      const bindEvents = function() {
+         DOM.arrowDownButton.on("click", toggleNavMenu);
+         $(document).on("click", handleDocumentClick);
+         DOM.alertCloseButton.on("click", closeAlertBox)
+   
+         setTimeout(hideAlert, 4 * 1000);
+      }
+   
+      const bindMasks = function() {
+         DOM.inputs.CEP.mask("00000-000");
+         DOM.inputs.CPF.mask("000.000.000-00");
+         DOM.inputs.telefone.mask("(00) 0000-0000");
+      }
+
+      const toggleNavMenu = function() {
+         !DOM.navMenu.hasClass("active") ?
+            DOM.navMenu.show().addClass("active") :
+            DOM.navMenu.fadeOut(100).removeClass("active");
+      }
+
+      const handleDocumentClick = function(e) {
+         if (!$(e.target).hasClass("js-arrow-down-button") &&
+             !$(e.target).parent().hasClass("js-nav-menu") &&
+             !$(e.target).parent().parent().hasClass("js-nav-menu")) {
+               if (DOM.navMenu.hasClass("active")) 
+                  DOM.navMenu.fadeOut(100).removeClass("active");
+         }
+      }
+   
+      const hideAlert = function() { DOM.alertBox.hide(); }
+   
+      const closeAlertBox = function() {
+         $(this).parent().hide().delay(100).remove();
+      }
+   
+      const init = function() {
+         bindEvents();
+         bindMasks();
+      }
+   
+      init();
+   });
+});
